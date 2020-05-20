44a093d936ffc4753fe53487bc3cf935668de84e
Robert Merrill <robgmerrill@gmail.com>
2020-05-20 09:22:51 -0700

    Use context for light and dark theme toggle

---
+++ src/components/Booklis.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
+
+class Booklist extends Component {
+  static contextType = ThemeContext;
+
+  render() {
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
+    return (
+      <div
+        className='book-list'
+        style={{ color: theme.syntax, background: theme.bg }}
+      >
+        <ul>
+          <li style={{ background: theme.ui }}>The way of Kings</li>
+          <li style={{ background: theme.ui }}>The name of the wind</li>
+          <li style={{ background: theme.ui }}>the final empire</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Booklist;
---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
+
+class Navbar extends Component {
+  // static contextType = ThemeContext;
+
+  render() {
+    // console.log(this.context);
+    // this approach works for functional components as well.
+    return (
+      <ThemeContext.Consumer>
+        {(context) => {
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark;
+          return (
+            <nav style={{ background: theme.ui, color: theme.syntax }}>
+              <h1>Context App</h1>
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+              </ul>
+            </nav>
+          );
+        }}
+      </ThemeContext.Consumer>
+    );
+  }
+}
+
+export default Navbar;
